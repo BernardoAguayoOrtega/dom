9c0a2d1459bf55f7243b480886fbea483ba89c2c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-03 09:31:02 -0600

    Dynamically dd CSS Styles

---
+++ 05.js
@@ -0,0 +1,10 @@
+// Dynamically add css styles
+const post = document.querySelector(".post");
+
+post.style.display = flex
+
+const post = document.querySelectorAll(".post");
+//post.style.margin = '30px';
+//post.classList.add('post');
+
+post.forEach(value => value.style.margin = '60px')
