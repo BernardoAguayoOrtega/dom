8990354d3bf0e916e198b9a417d2de72cfff2c58
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-02 18:45:10 -0600

    QUery selectors

---
+++ 02.js
@@ -0,0 +1,15 @@
+// const el = document.getElementById('home')
+// console.log(el)
+
+// const links = document.querySelectorAll('a');
+// // console.log(link);
+
+// links.forEach(link => {
+//   if (link.matches('a[href="/login"]')) {
+//     const loginLink = link;
+//     console.log(loginLink);  
+//   }
+// })
+
+const divs = document.getElementsByTagName('div')
+// console.log(divs);
